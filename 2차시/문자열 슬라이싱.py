55014d4ff26fadfba4224242415cfd41b6edb0e5
symarieChoi <symarie.choi@gmail.com>
2024-09-18 16:13:29 +0900

    이름 변경

--- 2차시/문자열 슬라이싱.py
+++
@@ -1,7 +0,0 @@
-code_num="990229-1234567"
-print("성별 식별번호: ", code_num[7])
-print("생년월일: ", code_num[0:6]) 
-print("생년월일: ", code_num[:6])
-print("생년월일: ", code_num[7:])
-print("생년월일: ", code_num[-7:])
-print("생년월일: ", code_num[-14])
